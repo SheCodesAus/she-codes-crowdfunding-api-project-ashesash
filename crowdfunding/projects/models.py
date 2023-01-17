dd18944bc9586d226a089cf4a5369d35780dd55c
ashesash <aishwarya.s@live.com>
2023-01-17 18:40:10 +1100

    new users app

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class Project(models.Model):
     title=models.CharField(max_length=200)
@@ -7,7 +10,11 @@ class Project(models.Model):
     image=models.URLField()
     is_open=models.BooleanField()
     date_created=models.DateTimeField(auto_now_add=True)
-    owner=models.CharField(max_length=200)
+    owner=models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='owner_projects'
+    )
 
 class Pledge(models.Model):
     amount = models.IntegerField()
@@ -18,5 +25,9 @@ class Pledge(models.Model):
         on_delete=models.CASCADE,
         related_name='pledges'
     )
-    supporter = models.CharField(max_length=200)
+    supporter = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='supporter_pledges'
+    )
 
