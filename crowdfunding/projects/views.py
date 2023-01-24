5ff183c689dd1c97742c3d416023d557ce133af8
ashesash <aishwarya.s@live.com>
2023-01-24 18:41:10 +1100

    pre-deployment

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -4,9 +4,13 @@ from rest_framework.response import Response
 from .models import Project, Pledge
 from .serializers import ProjectSerializer, ProjectDetailSerializer, PledgeSerializer
 from django.http import Http404
-from rest_framework import status, generics
+from rest_framework import status, generics, permissions
+from .permissions import IsOwnerOrReadOnly
+
 
 class ProjectList(APIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
     def get(self, request):
         projects = Project.objects.all()
         serializer = ProjectSerializer(projects, many=True)
@@ -19,16 +23,23 @@ class ProjectList(APIView):
             return Response(
                 serializer.data,
                 status=status.HTTP_201_CREATED
-                )
+            )
         return Response(
             serializer.errors,
             status=status.HTTP_400_BAD_REQUEST
         )
-    
+
 class ProjectDetail(APIView):
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly
+    ]
+
     def get_object(self, pk):
         try:
-            return Project.objects.get(pk=pk)
+            project = Project.objects.get(pk=pk)
+            self.check_object_permissions(self.request, project)
+            return project
         except Project.DoesNotExist:
             raise Http404
 
@@ -36,6 +47,20 @@ class ProjectDetail(APIView):
         project = self.get_object(pk)
         serializer = ProjectDetailSerializer(project)
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        project = self.get_object(pk)
+        data = request.data
+        serializer = ProjectDetailSerializer(
+            instance=project,
+            data=data,
+            partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data)
+
+
 class PledgeList(generics.ListCreateAPIView):
     # def get(self, request):
     #     pledges = Pledge.objects.all()
--- crowdfunding/users/serializers.py
+++ crowdfunding/users/serializers.py
@@ -5,7 +5,9 @@ class CustomUserSerializer(serializers.Serializer):
     id = serializers.ReadOnlyField()
     username = serializers.CharField(max_length=200)
     email = serializers.EmailField()
+    password = serializers.CharField(write_only = True)
 
     def create(self, validated_data):
-        return CustomUser.objects.create(**validated_data)
+        return CustomUser.objects.create_user(**validated_data)
+
         
